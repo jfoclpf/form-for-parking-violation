80486772936555be249dda71f69046b318f9d5ad
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-02-24 00:39:35 +0100

    starts to work on the server side to collect some data from client on the parking violations

---
+++ server/app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const mysql = require('mysql') // module to get info from database
+const debug = require('debug')('app:submitUserInput')
+const sqlFormatter = require('sql-formatter')
+const DBInfo = require('DBcredentials')
+
+const app = express()
+const port = 3035
+
+app.post('/passeio_livre/serverapp', function (req, res) {
+  // object got from POST
+  var databaseObj = req.body.databaseObj
+
+  debug(databaseObj)
+  debug('\nInserting user data into ' +
+                'database table ' + DBInfo.database + '->' + DBInfo.db_tables.denuncias)
+
+  // builds sql query to insert user data
+  var queryInsert = 'INSERT INTO ' + DBInfo.db_tables.denuncias + ' ('
+  var databaseKeys = Object.keys(databaseObj)
+  for (let i = 0; i < databaseKeys.length; i++) {
+    queryInsert += databaseKeys[i] + (i !== databaseKeys.length - 1 ? ', ' : ')')
+  }
+  queryInsert += ' ' + 'VALUES('
+  for (let i = 0; i < databaseKeys.length; i++) {
+    queryInsert += '\'' + databaseObj[databaseKeys[i]] + '\'' + (i !== databaseKeys.length - 1 ? ', ' : ')')
+  }
+  debug(sqlFormatter.format(queryInsert))
+
+  var db = mysql.createConnection(DBInfo)
+
+  db.connect(function (err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack)
+      throw err
+    }
+    debug('User ' + DBInfo.user + ' connected successfully to database ' + DBInfo.database + ' at ' + DBInfo.host)
+  })
+
+  db.query(queryInsert, function (err, results, fields) {
+    if (err) {
+      // error handling code goes here
+      debug('Error inserting user data into database: ', err)
+      res.status(501).send(JSON.stringify(err))
+    } else {
+      debug('User data successfully added into ' +
+                        'database table ' + DBInfo.database + '->' + DBInfo.db_tables.denuncias + '\n\n')
+      debug('Result from db query is : ', results)
+      res.send(results)
+    }
+  })
+
+  db.end()
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
