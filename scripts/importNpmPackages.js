5609b4727b564062ea32b821a0577880a269d6b3
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-06-06 23:50:26 +0200

    update npm packages when preparing app

---
+++ scripts/importNpmPackages.js
@@ -0,0 +1,30 @@
+const fse = require('fs-extra')
+const path = require('path')
+
+var projectRoot
+
+module.exports = function (context) {
+  console.log(context.hook + ': Importing npm packages files')
+
+  projectRoot = path.resolve(path.dirname(context.scriptLocation), '..')
+  console.log('Project root directory:', projectRoot)
+  copyFile('jquery', path.join('dist', 'jquery.min.js'), path.join('www', 'js', 'res', 'jquery.min.js'))
+}
+
+function copyFile (npmPackage, // oficial name of the npm package from which the file is to be copied from
+  fileRelativePath, // file path with respect to the main directory of the npm package (node_modules/<package>/)
+  destFilePath) { // file's path to where it is copied, relative to the project bin/ directory
+  // trick to get the npm module main directory
+  // https://stackoverflow.com/a/49455609/1243247
+  const packageDirFullpath = path.dirname(require.resolve(path.join(npmPackage, 'package.json')))
+  const fileOriginFullPath = path.join(packageDirFullpath, fileRelativePath)
+  const fileDestFullPath = path.join(projectRoot, destFilePath)
+
+  fse.copySync(fileOriginFullPath, fileDestFullPath)
+
+  const consoleMsg = npmPackage + ': ' +
+    path.relative(projectRoot, fileOriginFullPath) + ' -> ' +
+    path.relative(projectRoot, fileDestFullPath)
+
+  console.log(consoleMsg)
+}
