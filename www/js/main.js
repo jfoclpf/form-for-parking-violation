15a95c31e706c54fd536f5501f8546d5865de235
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-06-06 23:50:39 +0200

    update

--- www/js/main.js
+++ www/js/main.js
@@ -2,7 +2,7 @@
 
 /* global $, cordova */
 
-var DEBUG = false
+var DEBUG = true
 
 /* tries to use built-in browser plugin to authentication;
 when false uses OS default browser with a simple url link;
