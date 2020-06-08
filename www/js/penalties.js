3593e931c9bb23773655bb558d390fc73bc8bd4a
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-06-08 11:27:08 +0200

    solve issue #44

--- www/js/penalties.js
+++ www/js/penalties.js
@@ -72,6 +72,11 @@ app.penalties = (function (thisModule) {
       description: 'num lugar reservado ao estacionamento exclusivo de determinados veículos',
       law_article: 'da alínea f) do n.º 1 do art.º 50.º, com referência ainda à alínea g) do n.º 2 do art.º 164.º do Código da Estrada'
     },
+    linha_amarela_ou_contínua: {
+      select: 'Junto a linha amarela ou contínua na berma da estrada',
+      description: 'na faixa de rodagem sobre ou a menos de 3 metros de uma linha longitudinal contínua',
+      law_article: 'da alínea g) do n.º 1 do art.º 49.º do Código da Estrada, com referência ainda ao sinal M12 do n.º 1 do art.º 62.º do Regulamento de Sinalização do Trânsito'
+    },
     residentes_apenas: {
       select: 'Estacionamento indevido em zona de residentes',
       description: 'num lugar em zona de residentes sem que tenha o correspondente título que a habilita a tal',
