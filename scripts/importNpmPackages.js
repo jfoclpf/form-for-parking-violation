90ee2852ac9e809e452aaa37a0e74ba642f28cbb
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-06-08 10:37:13 +0200

    jAlert and css files from known packages updated automatically

--- scripts/importNpmPackages.js
+++ scripts/importNpmPackages.js
@@ -9,7 +9,12 @@ module.exports = function (context) {
   projectRoot = path.resolve(path.dirname(context.scriptLocation), '..')
   console.log('Project root directory:', projectRoot)
   copyFile('jquery', path.join('dist', 'jquery.min.js'), path.join('www', 'js', 'res', 'jquery.min.js'))
+
   copyFile('bootstrap', path.join('dist', 'js', 'bootstrap.min.js'), path.join('www', 'js', 'res', 'bootstrap.min.js'))
+  copyFile('bootstrap', path.join('dist', 'css', 'bootstrap.min.css'), path.join('www', 'css', 'res', 'bootstrap.min.css'))
+
+  copyFile('jAlert', path.join('dist', 'jAlert.min.js'), path.join('www', 'js', 'res', 'jAlert.min.js'))
+  copyFile('jAlert', path.join('dist', 'jAlert.css'), path.join('www', 'css', 'res', 'jAlert.css'))
 }
 
 function copyFile (npmPackage, // oficial name of the npm package from which the file is to be copied from
