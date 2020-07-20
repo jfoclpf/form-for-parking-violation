ee0422446db7df2403c55641734c8e8cc37e6f74
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-07-20 23:19:55 +0200

    run on device script include new path

--- runOnDevice.sh
+++ runOnDevice.sh
@@ -2,6 +2,7 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export ANDROID_HOME=/home/joao/Android/Sdk/
+export ANDROID_SDK_ROOT=/home/joao/Android/Sdk/
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 
