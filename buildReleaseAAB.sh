da9834eb65de34599179122a7c701da84a5c8c9d
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-08-24 12:16:26 +0200

    improve building of aab script

--- buildReleaseAAB.sh
+++ buildReleaseAAB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-export ANDROID_HOME=/home/joao/Android/Sdk/
+export ANDROID_SDK_ROOT=/home/joao/Android/Sdk/
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 cd "${0%/*}"
@@ -11,20 +11,18 @@ source keys/keyPassword
 
 cordova clean
 
-cordova build android --prod --release
+cordova build android --prod --release &&
 
-cd platforms/android/ && ./gradlew bundle
+cd platforms/android/ && ./gradlew bundle &&
 
 cd ../../
 
 cp keys/autocosts.keystore platforms/android/app/build/outputs/bundle/release/
 cd platforms/android/app/build/outputs/bundle/release/
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore autocosts.keystore -storepass $PASS app.aab autocosts
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore autocosts.keystore -storepass $PASS app-release.aab autocosts &&
 
 cd ../../../../../../..
 rm formParkingViolation.aab
 
-cp platforms/android/app/build/outputs/bundle/release/app.aab ./formParkingViolation.aab  
-
-
+cp platforms/android/app/build/outputs/bundle/release/app-release.aab ./formParkingViolation.aab
