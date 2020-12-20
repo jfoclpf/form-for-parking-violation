6e5085c713c236225536140d9fbd069cb2605914
jfoclpf <joao.pimentel.ferreira@gmail.com>
2020-12-20 23:26:57 +0100

    allow to resend a reminder to authority about a past ocurrence

--- www/js/historic.js
+++ www/js/historic.js
@@ -37,54 +37,192 @@ app.historic = (function (thisModule) {
 
     if (data.length === 0) {
       $('#historic').append('<center>Sem resultados</center>')
-    } else {
-      $('#historic').append(`
-        <h4>Histórico de ocorrências</h4>
-        <span class="note">Pressione nas ocorrências para ver as imagens</span>
-      `)
+      return
+    }
 
-      // since the results are stored as they are submitted, they are ordered by time
-      // we want to show on top the most recent ones, i.e., the last on the array
-      for (var i = data.length - 1; i >= 0; i--) {
-        const el = data[i]
-        $('#historic').append(`
-          <div class="row border-element historic_element" data-index="${i}">
-            <div class="col-xs-2 col-sm-2 form-group">
+    $('#historic').append(`
+      <h4>Histórico de ocorrências</h4>
+      <span class="note">Pressione nas ocorrências para ver as imagens</span>
+    `)
+
+    // since the results are stored as they are submitted, they are ordered by time
+    // we want to show on top the most recent ones, i.e., the last on the array
+    for (var i = data.length - 1; i >= 0; i--) {
+      const el = data[i]
+      $('#historic').append(`
+        <div class="p-3 border-element historic_element" data-index="${i}">
+          <div class="row">
+            <div class="col-9">
               ${el.carro_marca} ${el.carro_modelo} na ${el.data_local} n. ${el.data_num_porta}, ${el.data_concelho},
               no dia ${(new Date(el.data_data)).toLocaleDateString('pt-PT')} às ${el.data_hora.slice(0, 5)}
             </div>
-            <div class="col-xs-2 col-sm-2 form-group">
-              Matrícula: ${el.carro_matricula}<br>
-              Infração: ${app.penalties.getShortDescription(el.base_legal)}<br>
-              Autoridade: ${el.autoridade}
+            <div class="col">
+              <button class="btn btn-primary btn-sm m-1 history-refresh-button" data-index="${i}"><i class="fa fa-refresh"></i></button>
+              <button class="btn btn-primary btn-sm m-1 history-check-button" data-index="${i}"><i class="fa fa-check"></i></button>
             </div>
-          </div>`
-        )
-      }
-
-      // shows or hides photos when the div historic entry is clicked
-      $('#historic .historic_element').click(function () {
-        const i = parseInt($(this).data('index'))
-
-        if ($(this).find('img').length === 0) { // no image found, adds images
-          const $photos = $('<div class="historic_photos"></div>')
-          $(this).append($photos)
-
-          // DB has 4 fields for images for the same DB entry: foto1, foto2, foto3 and foto4
-          for (var photoIndex = 1; photoIndex <= 4; photoIndex++) {
-            if (data[i]['foto' + photoIndex]) { // if that photo index exists in the DB entry
-              const fullImgUrl = requestImageUrl + data[i]['foto' + photoIndex]
-              // check if the image really exists
-              $.get(fullImgUrl).done(() => {
-                $photos.append(`<img src="${fullImgUrl}">`)
-              })
-            }
-          }
-        } else { // has already images, remove them
-          $(this).find('.historic_photos').remove()
-        }
-      })
+          </div>
+          <div>
+            Matrícula: ${el.carro_matricula}<br>
+            Infração: ${app.penalties.getShortDescription(el.base_legal)}<br>
+            Autoridade: ${el.autoridade}
+          </div>
+        </div>`
+      )
     }
+
+    // deals with button to send refresh message
+    $('#historic .history-refresh-button').click(function (event) {
+      event.stopPropagation()
+      const i = parseInt($(this).data('index'))
+      $.jAlert({
+        theme: 'dark_blue',
+        class: 'ja_300px',
+        content: 'Deseja enviar um lembrete à autoridade respetiva a propósito desta ocurrência?',
+        btns: [
+          {
+            text: 'Sim',
+            theme: 'green',
+            class: 'ja_button_with_icon',
+            onClick: function () {
+              sendReminderEmail(data[i])
+            }
+          },
+          {
+            text: 'Não',
+            theme: 'green',
+            class: 'ja_button_with_icon'
+          }
+        ]
+      })
+    })
+
+    // deals with button to set status as processed
+    $('#historic .history-check-button').click(function (event) {
+      event.stopPropagation()
+
+      const $thisButton = $(this)
+
+      if ($thisButton.hasClass('btn-primary')) {
+        $.jAlert({
+          theme: 'dark_blue',
+          class: 'ja_300px',
+          content: 'Deseja colocar esta denúncia como tratada e resolvida?',
+          btns: [
+            {
+              text: 'Sim',
+              theme: 'green',
+              class: 'ja_button_with_icon',
+              onClick: function () {
+                $thisButton.siblings('.history-refresh-button').hide()
+                $thisButton.removeClass('btn-primary').addClass('btn-success')
+              }
+            },
+            {
+              text: 'Não',
+              theme: 'green',
+              class: 'ja_button_with_icon'
+            }
+          ]
+        })
+      } else if ($thisButton.hasClass('btn-success')) {
+        $.jAlert({
+          theme: 'dark_blue',
+          class: 'ja_300px',
+          content: 'Deseja remarcar esta denúncia como não tratada e não resolvida?',
+          btns: [
+            {
+              text: 'Sim',
+              theme: 'green',
+              class: 'ja_button_with_icon',
+              onClick: function () {
+                $thisButton.siblings('.history-refresh-button').show()
+                $thisButton.removeClass('btn-success').addClass('btn-primary')
+              }
+            },
+            {
+              text: 'Não',
+              theme: 'green',
+              class: 'ja_button_with_icon'
+            }
+          ]
+        })
+      } else {
+        console.error('Error dealing with button', $thisButton)
+      }
+    })
+
+    // shows or hides photos when the div historic entry is clicked
+    $('#historic .historic_element').click(function () {
+      const i = parseInt($(this).data('index'))
+
+      if ($(this).find('img').length === 0) { // no image found, adds images
+        const $photos = $('<div class="historic_photos"></div>')
+        $(this).append($photos)
+
+        // DB has 4 fields for images for the same DB entry: foto1, foto2, foto3 and foto4
+        for (var photoIndex = 1; photoIndex <= 4; photoIndex++) {
+          if (data[i]['foto' + photoIndex]) { // if that photo index exists in the DB entry
+            const fullImgUrl = requestImageUrl + '/' + data[i]['foto' + photoIndex]
+            // check if the image really exists
+            $.get(fullImgUrl).done(() => {
+              $photos.append(`<img src="${fullImgUrl}">`)
+            })
+          }
+        }
+      } else { // has already images, remove them
+        $(this).find('.historic_photos').remove()
+      }
+    })
+  }
+
+  function sendReminderEmail (occurrence) {
+    // download images from server to cache to attach them in email
+    // DB has 4 fields for images for the same DB entry: foto1, foto2, foto3 and foto4
+    var photosDeferred = []
+    console.log('start sendReminderEmail')
+    var downloadFileToDevice = function (photoIndex, fullImgUrl, fileName) {
+      // externalCacheDirectory ("file:///storage/emulated/0/Android/data/com.form.parking.violation/cache/") is indeed normally internal memory,
+      // see https://www.reddit.com/r/Android/comments/496sn3/lets_clear_up_the_confusion_regarding_storage_in/
+      app.functions.downloadFileToDevice(fullImgUrl, fileName, cordova.file.externalCacheDirectory,
+        (err, localFileName) => {
+          if (err) {
+            photosDeferred[photoIndex].resolve(null)
+          } else {
+            photosDeferred[photoIndex].resolve(localFileName)
+          }
+        })
+    }
+
+    for (var photoIndex = 1; photoIndex <= 4; photoIndex++) {
+      if (occurrence['foto' + photoIndex]) { // if that photo index exists in the DB entry
+        const fileName = occurrence['foto' + photoIndex]
+        const fileExtension = fileName.split('.').pop()
+        const fullImgUrl = requestImageUrl + fileName
+
+        photosDeferred[photoIndex] = $.Deferred()
+        downloadFileToDevice(photoIndex, fullImgUrl, `img${photoIndex}.${fileExtension}`)
+      }
+    }
+
+    $.when(...photosDeferred).done(function (/* arguments array */) {
+      var attachments = []
+      for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i]) {
+          attachments.push(arguments[i])
+        }
+      }
+      console.log(JSON.stringify(attachments, 0, 3))
+
+      var emailSubject = `[${occurrence.carro_matricula}] na ${occurrence.data_local}, ${occurrence.data_concelho} - Inquirição sobre estado processual da denúncia de estacionamento anteriormente efetuada`
+
+      cordova.plugins.email.open({
+        to: app.contactsFunctions.getEmailByFullName(occurrence.autoridade),
+        attachments: attachments, // file paths or base64 data streams
+        subject: emailSubject, // subject of the email
+        body: app.text.getReminderMessage(occurrence), // email body (for HTML, set isHtml to true)
+        isHtml: true // indicats if the body is HTML or plain text
+      })
+    })
   }
 
   thisModule.updateHistoric = updateHistoric
