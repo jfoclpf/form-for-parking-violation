a45e901fb4bc85d24577bf603655396574c61715
jfoclpf <joao.pimentel.ferreira@gmail.com>
2021-03-08 00:10:57 +0100

    include leaflet for embedding maps from OMS

--- hooks/importNpmPackages.js
+++ hooks/importNpmPackages.js
@@ -10,23 +10,26 @@ module.exports = function (context) {
 
   projectRoot = context.opts.projectRoot
   console.log(twoSpaces + 'Project root directory: ' + projectRoot)
-  copyFile('jquery', path.join('dist', 'jquery.min.js'), path.join('www', 'js', 'res', 'jquery.min.js'))
+  copyFile('jquery', path.join('dist', 'jquery.min.js'), path.join('js', 'res', 'jquery.min.js'))
 
-  copyFile('bootstrap', path.join('dist', 'js', 'bootstrap.min.js'), path.join('www', 'js', 'res', 'bootstrap.min.js'))
-  copyFile('bootstrap', path.join('dist', 'css', 'bootstrap.min.css'), path.join('www', 'css', 'res', 'bootstrap.min.css'))
+  copyFile('bootstrap', path.join('dist', 'js', 'bootstrap.min.js'), path.join('js', 'res', 'bootstrap.min.js'))
+  copyFile('bootstrap', path.join('dist', 'css', 'bootstrap.min.css'), path.join('css', 'res', 'bootstrap.min.css'))
 
-  copyFile('jAlert', path.join('dist', 'jAlert.min.js'), path.join('www', 'js', 'res', 'jAlert.min.js'))
-  copyFile('jAlert', path.join('dist', 'jAlert.css'), path.join('www', 'css', 'res', 'jAlert.css'))
+  copyFile('jAlert', path.join('dist', 'jAlert.min.js'), path.join('js', 'res', 'jAlert.min.js'))
+  copyFile('jAlert', path.join('dist', 'jAlert.css'), path.join('css', 'res', 'jAlert.css'))
+
+  copyFile('leaflet', path.join('dist', 'leaflet.js'), path.join('js', 'res', 'leaflet.js'))
+  copyFile('leaflet', path.join('dist', 'leaflet.css'), path.join('css', 'res', 'leaflet.css'))
 }
 
 function copyFile (npmPackage, // oficial name of the npm package from which the file is to be copied from
   fileRelativePath, // file path with respect to the main directory of the npm package (node_modules/<package>/)
-  destFilePath) { // file's path to where it is copied, relative to the project bin/ directory
+  destFilePath) { // file's path to where it is copied, relative to the project www/ directory
   // trick to get the npm module main directory
   // https://stackoverflow.com/a/49455609/1243247
   const packageDirFullpath = path.dirname(require.resolve(path.join(npmPackage, 'package.json')))
   const fileOriginFullPath = path.join(packageDirFullpath, fileRelativePath)
-  const fileDestFullPath = path.join(projectRoot, destFilePath)
+  const fileDestFullPath = path.join(projectRoot, 'www', destFilePath)
 
   fse.copySync(fileOriginFullPath, fileDestFullPath)
 
