345697735b44a87d9213d9f48f8b3bec765b8e25
jfoclpf <joao.pimentel.ferreira@gmail.com>
2021-11-26 22:43:11 +0100

    less verbose with template literals

--- www/js/text.js
+++ www/js/text.js
@@ -23,26 +23,24 @@ app.text = (function (thisModule) {
       const isCarModel = ($('#carmodel').val().replace(/^\s+|\s+$/g, '').length !== 0)
       let carmakeModelText
       if (isCarMake && isCarModel) {
-        carmakeModelText = 'de marca e modelo <b>' + $('#carmake').val() +
-          ' ' + $('#carmodel').val() + '</b>, '
+        carmakeModelText = `de marca e modelo <b>${$('#carmake').val()} ${$('#carmodel').val()}</b>, `
       } else if (isCarMake) {
-        carmakeModelText = 'de marca <b>' + $('#carmake').val() + '</b>, '
+        carmakeModelText = `de marca <b>${$('#carmake').val()}</b>, `
       } else if (isCarModel) {
-        carmakeModelText = 'de modelo <b>' + $('#carmodel').val() + '</b>, '
+        carmakeModelText = `de modelo <b>${$('#carmodel').val()}</b>, `
       } else {
         carmakeModelText = ''
       }
 
       const msg = getRandomGreetings() + ' da ' + getNameOfCurrentSelectedAuthority() + ';'
 
-      const msg1 = 'Eu, <b>' + $('#name').val() + '</b>,' +
-        ' com o <b>' + $('#id_type').val() + '</b> com o número <b>' + $('#id_number').val() + '</b> ' +
-        'e com residência em <b>' + $('#address').val() +
-        ', ' + $('#postal_code').val() + ', ' + $('#address_city').val() +
-        '</b>, venho por este meio,' + ' ' +
-        'ao abrigo do n.º 5 do artigo 170.º do Código da Estrada, ' +
-        'fazer a seguinte denúncia de contraordenação para que V. Exas. ' +
-        'levantem o auto respetivo e multem o infra-mencionado responsável.'
+      // see https://stackoverflow.com/a/37321145/1243247
+      const msg1 = `Eu, <b>${$('#name').val()}</b>, \
+        com o <b>${$('#id_type').val()}</b> com o número <b>${$('#id_number').val()}</b> \
+        e com residência em <b>${$('#address').val()}, ${$('#postal_code').val()}, ${$('#address_city').val()}</b>, \
+        venho por este meio, ao abrigo do n.º 5 do artigo 170.º do Código da Estrada, \
+        fazer a seguinte denúncia de contraordenação para que V. Exas. \
+        levantem o auto respetivo e multem o infra-mencionado responsável.`
 
       let msg2 = 'No passado dia <b>' +
         $.datepicker.formatDate("dd' de 'MM' de 'yy", $('#date').datepicker('getDate')) + '</b>' +
@@ -54,13 +52,15 @@ app.text = (function (thisModule) {
         ) // optional
 
       if (app.functions.isThis_iOS()) {
-        msg2 += `a viatura <b>${carmakeModelText}</b> cuja matrícula se encontra na foto em anexo, `
+        // apple doesn't allow car plates, considers it defamation
+        msg2 += `a viatura ${carmakeModelText}\
+          cuja matrícula se encontra na foto em anexo, `
       } else {
-        msg2 += 'a viatura com matrícula <b>' + carPlateStr + '</b> ' + carmakeModelText
+        msg2 += `a viatura com matrícula <b>${carPlateStr}</b> ${carmakeModelText}`
       }
 
       // penalities
-      msg2 += 'encontrava-se estacionada' + ' '
+      msg2 += 'encontrava-se estacionada '
       for (let i = 0; i < selectedPenalties.length; i++) {
         if (i !== selectedPenalties.length - 1) {
           msg2 += `${selectedPenalties[i].description}, em violação ${selectedPenalties[i].lawArticle}; `
@@ -69,16 +69,14 @@ app.text = (function (thisModule) {
         }
       }
 
-      const msg3 = 'Pode-se comprovar esta situação através' +
-        ' ' + ((app.photos.getPhotosUriOnFileSystem().length === 1) ? 'da fotografia anexa' : 'das fotografias anexas') +
-        ' ' + 'à presente mensagem eletrónica. ' +
-        'Juro pela minha honra que a informação supra citada é verídica.' +
-        ' ' + 'Recordo ainda, que ao abrigo do referido n.º 5 do artigo 170.º do Código da Estrada,' +
-        ' ' + 'a autoridade que tiver notícia por denúncia de contraordenação, levanta auto,' +
-        ' ' + 'não carecendo de presenciar tal contraordenação rodoviária, ' +
-        'situação a que se aplica o n.º 1 do mesmo artigo.' + '</b></b>' +
-        ' ' + 'Refiro ainda que me encontro plenamente disponível para participar na qualidade de testemunha' +
-        ' ' + 'no processo que vier a ser instaurado com referência à presente missiva.'
+      const msg3 = `Pode-se comprovar esta situação através \
+        ${((app.photos.getPhotosUriOnFileSystem().length === 1) ? 'da fotografia anexa' : 'das fotografias anexas')} \
+        à presente mensagem eletrónica. Juro pela minha honra que a informação supra citada é verídica. \
+        Recordo ainda, que ao abrigo do referido n.º 5 do artigo 170.º do Código da Estrada, \
+        a autoridade que tiver notícia por denúncia de contraordenação, levanta auto, \
+        não carecendo de presenciar tal contraordenação rodoviária, situação a que se aplica o n.º 1 do mesmo artigo. \
+        Refiro ainda que me encontro plenamente disponível para participar na qualidade de testemunha \
+        no processo que vier a ser instaurado com referência à presente missiva.`
 
       const message = msg + '<br><br>' + msg1 + '<br><br>' + msg2 + '<br><br>' + msg3 + '<br><br>' + getRegards() + '<br>'
 
